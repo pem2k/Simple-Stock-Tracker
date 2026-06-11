4e36f4fea6a6be019f94a12b7e339eadad45dc21
pem <mckilpar000@hotmail.com>
2026-06-10 20:03:22 -0700

    basic connection.js

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,1 +1,17 @@
-// Placeholder so git tracks this file
+import { MongoClient } from "mongodb";
+
+const client = new MongoClient(process.env.MONGO_URI, {
+  serverSelectionTimeoutMS: 5000,
+});
+
+let db;
+
+export async function connectDB() {
+  await client.connect();
+  db = client.db(process.env.MONGO_DB_NAME);
+  console.log("Connected to MongoDB:", process.env.MONGO_DB_NAME);
+}
+
+export function getDB() {
+  return db;
+}
