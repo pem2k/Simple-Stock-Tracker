ecaf36f040d196568c26155dd50707a2b4b44152
pem <mckilpar000@hotmail.com>
2026-06-12 16:11:15 -0700

    user sign up is completed and working with a db write to insomina

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,10 +1,37 @@
-// placeholder for login routes for git tracking
-// also need to write a middleware for route protection
+import express from "express";
+import { createUser /*findSafeUserByUsername*/ } from "../modules/users.js";
+//import bcrypt from "bcrypt";
 
-// routes to write
+const router = express.Router();
+
+// need to write a middleware for route protection
+
+router.post("/signup", async (req, res) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res
+      .status(400)
+      .json({ error: "username and password are required" });
+  }
+  try {
+    const user = await createUser(username, password);
+
+    res.status(201).json({
+      username: user.username,
+      message: "account created. Please log in.",
+    });
+  } catch (err) {
+    return res.status(500).json({ error: `Unable to create account, ${err} ` });
+  }
+});
+
+// routes to write, these are all posts
 
 // sign up
 
 //login
 
 //logout
+
+export default router;
