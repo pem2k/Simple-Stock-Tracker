fcecdd9bb83e5f64ca59de5800cb6adc83d77ab0
pem <mckilpar000@hotmail.com>
2026-06-12 12:13:14 -0700

    added create user function to users module, also lowered SALT_ROUNDS constant, makes bcrypt take forever.

--- src/modules/users.js
+++ src/modules/users.js
@@ -2,16 +2,53 @@ import bcrypt from "bcrypt";
 import { getDB } from "../db/connection.js";
 
 const USERS_COLLECTION = "users";
-const SALT_ROUNDS = 20;
+const SALT_ROUNDS = 12;
 
 //find user - mongo id or username, use this to
 // check if user exists on login, pull stock data from acc, etc.
 // read operation - get
 
+export async function findUserByUsername(username) {
+  return getDB().collection(USERS_COLLECTION).findOne({ username });
+}
+
+//TODO: Check if I actually need this function, createuser will obv use
+//username because it's provided by user, holding onto it for now but may
+// delete
+
+/*
+export async function findUserByID(_id) {
+  return getDB().collection(USERS_COLLECTION).findOne({ _id });
+}
+  */
+
 //create user - this is going to be used on the signup route,
 //should contain logic that the username provided isn't taken
 //writes to mongo db, creating a new user, password hashing also happens here.
 // add operation - post
+export async function createUser(username, password) {
+  const userCheck = await findUserByUsername(username);
+
+  if (userCheck) {
+    // this error will bubble to the route where we can return a status code
+    // in the catch
+    throw new Error("Error, username taken");
+  }
+  // Hash password here, data flow is user sends password over https:
+  // as plain text, it's hashed by bcrypt and I only save the hahs
+  // will need to go back through server.js and setup session middle ware to test these once
+  // routes are up.
+  const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS);
+  const newUser = await getDB().collection(USERS_COLLECTION).insertOne({
+    username,
+    hashedPassword,
+  });
+
+  return {
+    _id: newUser.insertedId,
+    username: username,
+  };
+}
 
 //login verification - checks pw hash and username match,
 // only returns user id and username.
@@ -23,11 +60,3 @@ const SALT_ROUNDS = 20;
 //we can avoid a ton of alpaca calls, though we should be fine at a 60/call
 // a second limit.
 // login verification - will be called by a post
-
-export async function findUserByUsername(username) {
-  return getDB().collection(USERS_COLLECTION).findOne({ username });
-}
-
-export async function findUserByID(_id) {
-  return getDB().collection(USERS_COLLECTION).findOne({ _id });
-}
